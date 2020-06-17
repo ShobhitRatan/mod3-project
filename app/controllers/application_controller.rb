a6b6a8d65a4eed884a525745690f32088b7dcef5
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-06-17 16:14:54 -0400

    added route for / and added index.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+    def home
+        render 'index.html'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/api', to: 'api#getRandom'
+  get '/', to: 'application#home'
   resources :pieces
   resources :scores
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
