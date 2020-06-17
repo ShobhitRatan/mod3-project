91f82028ecddec3922ad692be6723dfdee141a06
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-06-17 16:20:02 -0400

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root :to=> 'application#home'
   get '/api', to: 'api#getRandom'
-  get '/', to: 'application#home'
   resources :pieces
   resources :scores
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
