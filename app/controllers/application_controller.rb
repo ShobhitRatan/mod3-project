64753dd5a08ad3a40419ff43136762e1ef22f7a1
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-06-17 16:41:12 -0400

    changed home route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-    def home
+    def homeRoute
         render 'index.html'
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root :to=> 'application#home'
+  root :to=> 'application#homeRoute'
   get '/api', to: 'api#getRandom'
   resources :pieces
   resources :scores
